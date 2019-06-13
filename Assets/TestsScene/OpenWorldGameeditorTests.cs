d645fa390da9ca34417d3db1a5ad2bede2a83401
zacharytafe <36681233+zacharytafe@users.noreply.github.com>
2019-06-13 15:27:32 +1000

    Added stuff.

---
+++ Assets/TestsScene/OpenWorldGameeditorTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class OpenWorldGameeditorTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void OpenWorldGameeditorTestsSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            var player = GameObject.Find("Player");
+            Assert.IsTrue(player != null);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator OpenWorldGameeditorTestsWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
